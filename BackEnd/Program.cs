4a468db7bfe9f9d02363c64d5fa84bcce0de3a5c
Mathilde Ninous <mathildeninous@gmail.com>
2023-03-17 18:06:16 +0100

    rendu intermedaire

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -1,9 +1,30 @@
 using System.Text.Json.Serialization;
 using BackEnd.Data;
 using BackEnd.Models;
+using Microsoft.AspNetCore.HttpLogging;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpLogging(logging =>
+{
+    // Customize HTTP logging here.
+    logging.LoggingFields = HttpLoggingFields.All;
+    logging.RequestHeaders.Add("sec-ch-ua");
+    logging.ResponseHeaders.Add("my-response-header");
+    logging.MediaTypeOptions.AddText("application/javascript");
+    logging.RequestBodyLogLimit = 4096;
+    logging.ResponseBodyLogLimit = 4096;
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:19000", "https://localhost:19000").AllowAnyHeader().AllowAnyMethod();
+                      });
+});
 /* Ignore circular references when serializing objects into JSON*/
 builder.Services.AddControllersWithViews().AddJsonOptions(x =>
     x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
@@ -12,6 +33,8 @@ builder.Services.AddControllersWithViews().AddJsonOptions(x =>
 builder.Services.AddDbContext<BackEndContext>();
 
 var app = builder.Build();
+
+app.UseHttpLogging();
 
 // Seed data into DB
 SeedData.init();
@@ -23,12 +46,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseHttpLogging();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
 
 app.MapControllerRoute(
